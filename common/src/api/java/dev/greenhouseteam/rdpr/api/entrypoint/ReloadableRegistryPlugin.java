2ebdf709d5238dfafcf8f9490c3c5b3d7f01e834
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-12-04 00:53:34 +1100

    Fix incorrect comments in ReloadableRegistryPlugin

--- common/src/api/java/dev/greenhouseteam/rdpr/api/entrypoint/ReloadableRegistryPlugin.java
+++ common/src/api/java/dev/greenhouseteam/rdpr/api/entrypoint/ReloadableRegistryPlugin.java
@@ -4,11 +4,9 @@ import dev.greenhouseteam.rdpr.api.IReloadableRegistryCreationHelper;
 
 /**
  * Called at init to create reloadable registries.
- * To load this on Fabric or Quilt, you'll have to add your plugin class to the 'reloadabledatapackregistries' entrypoint.
+ * To load this on Fabric or Quilt, you'll have to add your plugin class to the 'rdpr' entrypoint.
  * To load this on NeoForge, use the ReloadableRegistryEvent on the mod event bus.
  */
 public interface ReloadableRegistryPlugin {
-    /**
-     */
     void createContents(IReloadableRegistryCreationHelper helper);
 }
